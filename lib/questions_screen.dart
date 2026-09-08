f80e0228ba98f4045fc6e29155e2fd8cb7f56ba3
nathan-renard <aimbire@proton.me>
2026-09-08 16:21:53 -0700

    Setting up a new screen.

---
+++ lib/questions_screen.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class QuestionsScreen extends StatefulWidget {
+  const QuestionsScreen({super.key});
+
+  @override
+  State<QuestionsScreen> createState() {
+    return _QuestionsScreenState();
+  }
+}
+
+class _QuestionsScreenState extends State<QuestionsScreen> {
+  @override
+  Widget build(context) {
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text('The questions screen'),
+          SizedBox(height: 30),
+          ElevatedButton(
+            onPressed: () {},
+            child: Text('Answer 1'),)
+        ],
+      ),
+    );
+  }
+}
